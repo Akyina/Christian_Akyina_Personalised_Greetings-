6906641e74735e3493b20021fcb183f3160e207f
Akyina <82227047+Akyina@users.noreply.github.com>
2024-11-23 07:28:47 +0000

    Update main.tf

--- terraform/personalized_greetings/main.tf
+++ terraform/personalized_greetings/main.tf
@@ -2,6 +2,6 @@ module "ec2_instance" {
   source        = "./../modules/ec2/"
   ami_id        = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
-  region        = "us-west-2"
+  region        = "us-east-1"
   instance_name = "my_terraform_ec2"
 }
