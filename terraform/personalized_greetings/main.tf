6857086ae8328f03489c38e59141b7bbc0c5634b
Ubuntu <ubuntu@ip-172-31-16-220.ec2.internal>
2024-11-23 07:18:34 +0000

    added a new folder to keep different modules for vpc

--- terraform/personalized_greetings/main.tf
+++ terraform/personalized_greetings/main.tf
@@ -1,5 +1,5 @@
 module "ec2_instance" {
-  source        = "./../modules/"
+  source        = "./../modules/ec2/"
   ami_id        = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   region        = "us-west-2"
