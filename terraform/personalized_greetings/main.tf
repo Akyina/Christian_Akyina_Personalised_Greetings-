de36293efd4362d7ed64c7dd8d068240c4e3a076
Ubuntu <ubuntu@ip-172-31-22-35.ec2.internal>
2024-12-15 02:04:16 +0000

    added lookup function and workspace in to the module

--- terraform/personalized_greetings/main.tf
+++ terraform/personalized_greetings/main.tf
@@ -11,7 +11,7 @@ terraform {
 module "ec2_instance" {
   source        = "./../modules/ec2/"
   ami_id        = "ami-04a81a99f5ec58529"
-  instance_type = "t2.micro"
+  instance_type = instance_type = lookup({dev = "t2.micro", prod = "t3.small"}, terraform.workspace, "m5.large")
   region        = "us-east-1"
   instance_name = "my_terraform_ec2"
 }
@@ -20,11 +20,11 @@ module "ec2_instance" {
 module "vpc" {
   source               = "../modules/vpc"
   cidr_block           = "10.0.0.0/16"
-  vpc_name             = "Personalized-Greetings-VPC"
+  vpc_name             = lookup({dev = "Dev_Personalized-Greetings-VPC", prod = "prod_Personalized-Greetings-VPC"}, terraform.workspace, "test_Personalized-Greetings-VPC")
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   default_tags = {
-    Environment = "Dev"
+    Environment = lookup({dev = "Dev_Personalized-Greetings-VPC", prod = "prod_Personalized-Greetings-VPC"}, terraform.workspace, "test_Personalized-Greetings-VPC")
     Project     = "Personalized-Greetings"
   }
 }
